9f5b6b166556f9d7572420a9138d3a64e6daae85
Collins Kariuki <collinsmunkary@gmail.com>
2023-06-16 16:08:12 +0300

    RESTful comments index

--- index.js
+++ index.js
@@ -4,8 +4,43 @@ const express = require('express')
 const app = express();
 //require body parsing middleware
 const bodyParser = require('body-parser')
+// Require the path that will be used to set paths to views
+const path = require('path')
+
+// set a path to the views directory
+app.set('views', path.join(__dirname, 'views'))
+
+// set the templating engine to be ejs
+app.set('view engine', 'ejs');
+
 
 app.use(bodyParser.urlencoded({extended: false}))
+
+// create comments
+const comments = [
+    {
+        username: "Todd",
+        comment: "lol that is so funny!"
+    },
+    {
+        username: "Skyler",
+        comment: "I like to go birdwatching with my dog"
+    },
+    {
+        username: "Sk8erBoi",
+        comment: "Plz delete your account, Todd"
+    },
+    {
+        username: "onlysayswoof",
+        comment: "woof woof woof"
+    }
+]
+
+// the base url for this resource
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
+
 
 // add basic get route
 app.get('/tacos', (req, res) => {
